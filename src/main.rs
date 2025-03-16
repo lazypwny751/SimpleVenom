bb67bbdb093933aa57f198988f2736381a4379c0
lazypwny751 <testfordnshijacking@gmail.com>
2025-03-16 07:22:25 +0300

    init main screen

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,15 @@ use clap::Parser;
 use args::Args;
 use exploit::options::TaskStruct;
 use i10n::transcript::TranscriptinTable;
+use tui::master::run;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let args = Args::parse();
-
     println!("{}", TranscriptinTable::localize("hello world", &args.lang));
+
+    let terminal = ratatui::init();
+    let result = run(terminal, &args.lang);
+    ratatui::restore();
+
+    Ok(result?)
 }
